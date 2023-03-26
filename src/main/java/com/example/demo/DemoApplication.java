94dbd0f7267efb39bd189e24853b158cb6078ee2
NickSpirakis <45397574+NickSpirakis@users.noreply.github.com>
2023-03-26 10:58:13 -0500

    tryung num

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -21,10 +21,19 @@ public class DemoApplication extends HttpServlet{
 
 		return "Hello Azure!";
 	}
-
+'''
 	@RequestMapping("/num")
 		int num = Integer.parseInt(req.getParameter('num'));
 		
 		System.out.println("Your number is: " + num);
 	}
+'''
+
+	@RequestMapping(value = "/{num}", method = RequestMethod.GET)
+		public String showNumber(@PathVariable("num") String num, Model model)
+		{ 
+			model.addAttribute("num", num); 
+			return "number is: " + num;
+		}
+
 }
