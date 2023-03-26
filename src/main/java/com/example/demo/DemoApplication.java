bb9a077e5ba1b1dca4e3be0a3a7b6d6885ba974c
NickSpirakis <45397574+NickSpirakis@users.noreply.github.com>
2023-03-26 11:53:52 -0500

    updated service

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @SpringBootApplication
 @RestController
 public class DemoApplication {
@@ -22,10 +27,26 @@ public class DemoApplication {
 
 		return "Hello Azure!";
 	}
-
+'''
+	@RequestMapping(value="/getNum", method=RequestMethod.GET)
+	public String getNum(@RequestParam(value="num") String num) {
+		PrintWriter out = res.getWriter();
+		out.println("Your number is: " + num);
+		//return "Hello Azure!";
+	}
+'''
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res)
+	 throws ServletException, IOException {
+		int num = Integer.parseInt(req.getParameter("num"));
+		PrintWriter out = res.getWriter();
+		out.println("Your number is: " + num);
+	}
+	'''
 	@RequestMapping(value="/getNum", method=RequestMethod.GET)
 	public String getNum(Model model, @RequestParam(value="num") String num) {
 		model.addAttribute("Number", num);
 		return "Hello Azure!";
 	}
+	'''
 }
