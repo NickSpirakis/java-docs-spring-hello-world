d31286c8d6ea310320756fbe41797adb5174bc67
NickSpirakis <45397574+NickSpirakis@users.noreply.github.com>
2023-03-26 13:17:02 -0500

    added extend

--- src/main/java/com/example/demo/getInput.java
+++ src/main/java/com/example/demo/getInput.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class getInput {
+public class getInput extends HttpServlet{
 
 	protected void service(HttpServletRequest req, HttpServletResponse res)
 	throws ServletException, IOException {
